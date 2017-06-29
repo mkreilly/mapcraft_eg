4911fb7ec620b76e54fdfab83445f9f1b9494b01
fscottfoti <fscottfoti@gmail.com>
2017-06-29 14:02:09 -0700

    edits to scripts

--- scripts/badata.py
+++ scripts/badata.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 cities_and_counties = yaml.load(open("cities_and_counties.yaml"))
 
+
 # read all the general plan spatial data in and merge it
 def merge_gp_spatial_data(cities_and_counties, path_format="{}/{}/*.geojson"):
     gdfs = []
@@ -21,8 +22,8 @@ def merge_gp_spatial_data(cities_and_counties, path_format="{}/{}/*.geojson"):
     return gpd.GeoDataFrame(pd.concat(gdfs))
 
 
-# we store general plan data in a set of shapefiles and zoning attribuets in a csv
-# this method tells us which join keys are missing from each dataset
+# we store general plan data in a set of shapefiles and zoning attributes in
+# a csv this method tells us which join keys are missing from each dataset
 def diagnose_merge(df, gdf):
     print "Number of records in zoning data that have a shape to join to:"
     df["zoning_id"] = df.id  # need to rename so names don't clash in merge
@@ -30,16 +31,17 @@ def diagnose_merge(df, gdf):
     df["name"] = df.name.str.upper()
     gdf["city"] = gdf.city.str.upper().str.replace('_', ' ')
     df["city"] = df.city.apply(lambda n: n if "COUNTY" not in n else
-        "UNINCORPORATED " + n.replace(" COUNTY", ""))
+                               "UNINCORPORATED " + n.replace(" COUNTY", ""))
     gdf["general_plan_name"] = gdf.general_plan_name.str.upper()
 
     df["name"] = df.name.str.replace(r'^[0-9][0-9][0-9] - ', '')
-    gdf["general_plan_name"] = gdf.general_plan_name.str.replace(r'^[0-9][0-9][0-9] - ', '')
+    gdf["general_plan_name"] = gdf.general_plan_name.str.replace(
+        r'^[0-9][0-9][0-9] - ', '')
 
     df2 = pd.merge(df, gdf,
                    left_on=["city", "name"],
                    right_on=["city", "general_plan_name"])
-    print df2.columns
+
     missing = df[~df.zoning_id.isin(df2.zoning_id)]
     print "{} missing zoning ids (data written to missing_zoning_ids.csv".\
         format(len(missing))
@@ -58,19 +60,21 @@ MODE = options.mode
 if MODE == "merge_gp_data":
     print "Reading geojson data by juris"
     gdf = merge_gp_spatial_data(cities_and_counties)
-    print "Writing general plan data as shapefile"
+    print "Writing general plan data as csvfile"
     gdf.to_csv("merged_general_plan_data.csv")
 
 elif MODE == "diagnose_merge":
     print "Reading gp data"
     gdf = pd.read_csv("merged_general_plan_data.csv")
-    print gdf.head(1).transpose()
-    print gdf.columns
     df = pd.read_csv("zoning_lookup.csv")
+    # this file is not in this repo - it should be copied from the
+    # bayarea_urbansim repo soon we will generate a new zoning-parcel
+    # relationship file using this data
     df2 = pd.read_csv("2015_12_21_zoning_parcels.csv")
-    print len(df)
-    df = df[df.id.isin(df2.zoning_id)]  # drop rows which aren't linked to parcels
+    # drop rows which aren't linked to parcels
+    df = df[df.id.isin(df2.zoning_id)]
     df["Parcel Count"] = df2.zoning_id.value_counts().loc[df.id].values
-    df.to_csv("foo.csv")
-    print len(df)
     diagnose_merge(df, gdf)
+
+else:
+    print "Must pick a mode.  Options include merge_gp_data, diagnose_merge..."
