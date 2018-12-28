6b321b6a9f205fa97ba8d2b968e39313823d3fc0
Shimon Israel <sisrael@bayareametro.gov>
2018-12-28 13:51:14 -0800

    Script to manipulate parking data

---
+++ scripts/Import block group data.R
@@ -0,0 +1,24 @@
+
+# Import block group data.R
+# Read in CSV "2015" TAZ block group data from ACS 2013-2017, rather than having to run the API import step 
+# SI
+# December 17, 2018
+
+#Import CSVs getting class right for geographies (so as not to lose leading 0s)
+
+bg_df1 <- read.csv("X:/petrale/output/ACS 2013-2017 Block Group Vars1.csv",header=TRUE, stringsAsFactors = FALSE,
+                   colClasses = c("tract" = "character","state" = "character","county" = "character", "block.group" = "character"))
+
+
+bg_df2 <- read.csv("X:/petrale/output/ACS 2013-2017 Block Group Vars2.csv",header=TRUE, stringsAsFactors = FALSE,
+                   colClasses = c("tract" = "character","state" = "character","county" = "character", "block.group" = "character"))
+
+
+
+bg_df3 <- read.csv("X:/petrale/output/ACS 2013-2017 Block Group Vars3.csv",header=TRUE, stringsAsFactors = FALSE,
+                   colClasses = c("tract" = "character","state" = "character","county" = "character", "block.group" = "character"))
+
+# Rename block.group to block_group, so rest of ACS 2013-2017 create TAZ data for 2015.R code works
+
+ACS_BG_preraw <- ACS_BG_preraw %>%
+  rename("block_group" = "block.group")
